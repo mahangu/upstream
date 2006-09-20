a482cd6dfc8a8f7990a1f725c2611cc3157cb32e
ryanzeigler <ryanzeigler@de486275-911b-0410-b142-b35c017fc177>
2006-09-20 01:05:52 +0000

    UI querying functions are finished.

--- canoe/canoe.py
+++ canoe/canoe.py
@@ -24,23 +24,18 @@ import gtk
 import sys
 import functions
 
-STANDARD_MASK = 1 << 0
-VIDEO_MASK = 1 << 1
-NETWORK_MASK = 1 << 2
 
 class Canoe:
 	# Initialize the class
 	def __init__(self):
-		self.request_object = 0
-		self.continue_input = 1
-		
+		self.request_object = None
 	def query(self):
 		email_addr = self.email_addr()
 		if email_addr:
 			support_msg = self.support_msg()
 			if support_msg:
-				suport_mask = support_mask()
-				request_object = RequestObject(email_addr, support_msg, support_mask)				
+				support_type = self.support_type()
+				self.request_object = RequestObject(email_addr, support_msg, support_type)				
 				return True
 		# If the user cancelled at any point return false
 		return False
@@ -48,51 +43,130 @@ class Canoe:
 		
 		
 	def submit(self):
-		pass	
+		print "Executing submit"
+		if self.request_object:
+			self.request_object.dump_info()
+			
+			gtkWin = gtk.Window(gtk.WINDOW_TOPLEVEL)
+			gtkBox = gtk.VBox(False, 5);
+			gtkLabel = gtk.Label("Your request is being submitted, please be patient, this may take sometime")
+			gtkButtonBox = gtk.HButtonBox(False, 5);
+			gtkButton = gtk.Button("Close", gtk.STOCK_CLOSE);
+			
+			gtkWin.add(gtkBox);
+			gtkBox.pack_start(
+			 
+	def destroy_callback(self, widget, data = None):
+	
+	
+	def delete_event_callback(self, widget, event, data=None):
+		return True
 		
 	def email_addr(self):
 		# Create the dialog
 		dialog = gtk.Dialog("E-Mail Address", None, gtk.DIALOG_MODAL, (gtk.STOCK_CANCEL, gtk.RESPONSE_REJECT, gtk.STOCK_OK, gtk.RESPONSE_ACCEPT))
 		# Pack instruction and text-entry into the dialog
 		label = gtk.Label("Enter your e-mail address: ")
-		dialog.vbox.pack_start(label, True, False, 5);
+		dialog.vbox.pack_start(label, False, False, 5);
 		text_entry = gtk.Entry()
 		dialog.vbox.pack_start(text_entry, True, True, 5);
 		# Show the elements
-		label.show()
-		text_entry.show()
+		
+		dialog.set_default_size(400, dialog.get_property("default-height"))
+		dialog.show_all()
+		
 		if dialog.run() == gtk.RESPONSE_ACCEPT:
 			print "Debug MSG: Got email addr: %s" % text_entry.get_text()
+			dialog.destroy()
 			return text_entry.get_text()
 		else:
 			print "Debug MSG: Aborted on email addr:"
+			dialog.destroy()
 			return None
 		
 	def support_msg(self):
-		pass
+		dialog = gtk.Dialog("Problem Description", None, gtk.DIALOG_MODAL, (gtk.STOCK_CANCEL, gtk.RESPONSE_REJECT, gtk.STOCK_OK, gtk.RESPONSE_ACCEPT))
+		# Pack instruction and text-entry into the dialog
+		label = gtk.Label("Enter a message describing your problem: ")
+		dialog.vbox.pack_start(label, False, False, 5);
+		text_view = gtk.TextView()
+		text_view.set_wrap_mode(gtk.WRAP_WORD)
+		dialog.vbox.pack_start(text_view, True, True, 5);
+		# Show the elements		
+		dialog.set_default_size(400, 250)
+		dialog.show_all()
+		if dialog.run() == gtk.RESPONSE_ACCEPT:
+			# Extract text from the text view
+			m_buffer = text_view.get_buffer()
+			m_buffer_s_iter = m_buffer.get_start_iter()
+			m_buffer_e_iter = m_buffer.get_end_iter()
+			message = m_buffer.get_text(m_buffer_s_iter, m_buffer_e_iter)
+			print "Debug MSG: Got support message:\n%s" % message
+			dialog.destroy()
+			return message
+		else:
+			dialog.destroy()
+			return None
 		
-	def support_mask(self):
-		pass
+	def support_type(self):
+		dialog = gtk.Dialog("Problem Category", None, gtk.DIALOG_MODAL, (gtk.STOCK_CANCEL, gtk.RESPONSE_REJECT, gtk.STOCK_OK, gtk.RESPONSE_ACCEPT))
+		# Pack instruction and text-entry into the dialog
+		label = gtk.Label("Select all boxes relevant to your problem: ")
+		dialog.vbox.pack_start(label, False, False, 5)
+		# Create some check buttons
+		network_check = gtk.CheckButton("Network", False)
+		video_check = gtk.CheckButton("Video", False)
+		# Pack the check buttons into the vbox
+		dialog.vbox.pack_start(network_check, False, False, 5)
+		dialog.vbox.pack_start(video_check, False, False, 5)
 		
-class RequestObject:
-	def __init__(self, email_addr, support_msg, support_mask):
-		self.email_addr = email_addr
-		self.support_msg = support_msg
-		self.support_mask = support_mask
+		dialog.set_default_size(400, dialog.get_property("default-height"))
+		dialog.show_all()
 		
-	def set_email_addr(self, addr):
-		self.email_addr = addr
+		support_list = []
+		if dialog.run() == gtk.RESPONSE_ACCEPT:
+			support_list.append("standard")
+			if network_check.get_active():
+				support_list.append("network")
+			if video_check.get_active():
+				support_list.append("video")
+		# Mask is initialized to 0, so we can safely return it
+		# if the user didn't enter anything, since we will get false
+		print "Debug MSG: Mask: ", support_list
+		dialog.destroy()
+		return support_list
+		
+		
+class RequestObject(threading.Thread):
+	self._gtk_adjustment = 0
+	self._gtk_button = 0
+	self._gtk_label = 0
+	# Note: support_type should be a list
+	def __init__(self, email_addr, support_msg, support_type):
+		threading.Thread.__init__(self)
+		self._email_addr = email_addr
+		self._support_msg = support_msg
+		self._support_type = support_type
+		
 	
-	def set_support_msg(self, msg):
-		self.support_msg = msg
+	def set_gtk_adjustment(self, newValue):
+		self._gtk_adjustment = newValue	
+	
+	# Set the button that should be unlocked when this element is finished running
+	def set_gtk_button(self, newValue):
+		self._gtk_button = newValue
 		
-	def set_support_mask(self, mask):
-		self.support_type_mask = self.support_type_mask | mask
+	def set_gtk_label(self, newValue):
+		self._gtk_label = newValue
+	
+	def run(self):
+		pass
 		
 	def dump_info(self):
-		print "%s\n%s\n%b" % (self.email_addr, self.support_msg, self.support_type_mask)
+		print "%s\n%s\n%d" % (self._email_addr, self._support_msg, self._support_type)
 		
 if __name__ == "__main__":
 	print "Executing Canoe PyGTK frontend"	
 	canoe_obj = Canoe();
-	canoe_obj.query()
+	if canoe_obj.query():
+		canoe_obj.submit()
