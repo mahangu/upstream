df77b9437b03812ffe61d790f3e679a4a3c37481
jasonribeiro <jasonribeiro@de486275-911b-0410-b142-b35c017fc177>
2006-10-06 03:35:05 +0000

    Initial check-in of Canoe2 using glade

--- trunk/canoe/canoe.py
+++ trunk/canoe/canoe.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 #
-# Canoe - gtk frontend for the upstream
+# Canoe - gtk frontend using glade for Upstream
 # log file aggregator and report tool for *nix systems.
-# Copyright (C) 2006  Ryan Zeigler (zeiglerr@gmail.com)
+# Copyright (C) 2006  Jason Ribeiro <jason.ribeiro@gmail.com>
+# Based on canoe.py by Ryan Zeigler <zeiglerr@gmail.com>
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -18,203 +19,161 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.\
 
-import pygtk
-pygtk.require('2.0')
-import gtk
-import sys, time, threading, string, ConfigParser
+import sys
+import functions
+import submitmoduleloader
+try:
+	import pygtk
+	pygtk.require("2.0")
+except:
+	pass
+try:
+	import gtk
+	import gtk.glade
+except:
+	sys.exit(1)
 
-import functions, asyncsubmit, submitmoduleloader
+class CanoeGTK:
+	"""This is the canoe frontend for Upstream"""
+
+	def __init__(self, module_loader):
+		self.module_loader = module_loader
+
+		# Set the Glade file
+		self.gladefile = "canoe.glade"  
+		self.wTree = gtk.glade.XML(self.gladefile) 
+
+		self.sections_dict = self.__gui_add_logs()
+		self.module_combobox = self.__gui_add_modules()
+
+		# Create our dictionay and connect it
+		dic = { "on_btnNext_clicked" : self.btnNext_clicked,
+				"on_btnBack_clicked" : self.btnBack_clicked,
+				"on_btnCancel_clicked" : self.destroy,
+				"on_MainWindow_destroy" : self.destroy }
+		self.wTree.signal_autoconnect(dic)
 
 
+	def __gui_add_logs(self):
+		"""Create the checkboxes for selecting logs"""
+
+		vbox = self.wTree.get_widget("vbox_problems")
+		sections_dict = dict([(section, gtk.CheckButton(section, False)) for section in functions.get_conf_sections("list")])
+		for section in sections_dict:
+			vbox.pack_start(sections_dict[section])
+		vbox.show_all()
+		return sections_dict
+
+	def __gui_add_modules(self):
+		"""Create the combobox for the pastebin modules"""
+
+		vbox = self.wTree.get_widget("vbox_modules")
+		combobox = gtk.combo_box_new_text()
+		vbox.pack_start(combobox)
+		for mod in module_loader:
+			combobox.append_text(mod.module_name)
+		#self.submit_select_changed(module_loader[0].module_name)
+		vbox.reorder_child(combobox, 1)
+		combobox.connect("changed", self.submit_select_changed)
+		vbox.show_all()
+		return combobox
 
 
-def query():
-	email_addr = get_email_addr()
-	if email_addr:
-		support_msg = get_support_msg()
-		if support_msg:
-			support_logs = get_support_type()
-			if support_logs:				
-				module = get_submit_module()
-				if module:
-					return asyncsubmit.ThreadSubmit(module, email_addr, support_msg, support_logs, on_submit_complete, None)	
-	# If the user cancelled at any point return false
-	return None
+	# Callbacks
+	def submit_select_changed(self, widget):
+		label_name = self.wTree.get_widget("label_submit_name")
+		label_description = self.wTree.get_widget("label_submit_description")
+		label_url = self.wTree.get_widget("label_submit_url")
 
-	
-	
-def submit(request):
+		name = widget.get_active_text()
+		module = module_loader[name]
+
+		label_name.set_text(module.module_name)
+		label_description.set_text(module.module_description)
+		label_url.set_text(module.module_submit_url)
+
+	def btnBack_clicked(self, widget):
+		nb = self.wTree.get_widget("MainNotebook")
+		back = self.wTree.get_widget("btnBack")
+		next = self.wTree.get_widget("btnNext")
 		
-	global window
-	window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-	window.set_default_size(400, 80)
-	global label
-	label = gtk.Label("Your request is being submitted, please be patient")
-	global vbox
-	vbox = gtk.VBox(True)
-	global hbuttonbox
-	hbuttonbox = gtk.HBox(True)
-		
-	window.add(vbox)
-	vbox.pack_start(label, True, False, 0)
-	vbox.pack_start(hbuttonbox, True, False, 0)	
-	window.connect("delete_event", window_delete_event, request)
-	window.connect("destroy", window_destroy)	
-	window.set_title("Completing...")
-	window.show_all()		
-	request.start()
-	gtk.main()
-	
-def window_delete_event(widget, event, obs=None):
-	# Data should be a reference to observer
-	if request.isStarted() and not request.isAlive():
-		return False
-	else:
-		return True
-		
-def window_destroy(widget, data=None):
-	gtk.main_quit()
-		
-def get_email_addr():
-	# Create the dialog
-	dialog = gtk.Dialog("E-Mail Address", None, gtk.DIALOG_MODAL, (gtk.STOCK_CANCEL, gtk.RESPONSE_REJECT, gtk.STOCK_OK, gtk.RESPONSE_ACCEPT))
-	# Pack instruction and text-entry into the dialog
-	label = gtk.Label("Enter your e-mail address: ")
-	dialog.vbox.pack_start(label, False, False, 5);
-	text_entry = gtk.Entry()
-	dialog.vbox.pack_start(text_entry, True, True, 5);
-	# Show the elements
-	
-	dialog.set_default_size(400, dialog.get_property("default-height"))
-	dialog.show_all()
-	
-	if dialog.run() == gtk.RESPONSE_ACCEPT:
-		dialog.destroy()
-		return text_entry.get_text()
-	else:
-		dialog.destroy()
-		return None
-	
-def get_support_msg():
-	dialog = gtk.Dialog("Problem Description", None, gtk.DIALOG_MODAL, (gtk.STOCK_CANCEL, gtk.RESPONSE_REJECT, gtk.STOCK_OK, gtk.RESPONSE_ACCEPT))
-	# Pack instruction and text-entry into the dialog
-	label = gtk.Label("Enter a message describing your problem: ")
-	dialog.vbox.pack_start(label, False, False, 5);
-	text_view = gtk.TextView()
-	text_view.set_wrap_mode(gtk.WRAP_WORD)
-	dialog.vbox.pack_start(text_view, True, True, 5);
-	# Show the elements		
-	dialog.set_default_size(400, 250)
-	dialog.show_all()
-	if dialog.run() == gtk.RESPONSE_ACCEPT:
-		# Extract text from the text view
+		# Make the "back" button inactive if we are coming from the second page
+		if nb.get_current_page() is 1:
+			back.set_sensitive(False)
+
+		# Change the "finish" button to a "next" button if we are coming from the last page
+		if nb.get_current_page() is nb.get_n_pages()-1:
+			next.set_label("gtk-go-forward")
+
+		nb.prev_page()
+
+	def btnNext_clicked(self, widget):
+		nb = self.wTree.get_widget("MainNotebook")
+		back = self.wTree.get_widget("btnBack")
+		next = self.wTree.get_widget("btnNext")
+
+		# Run the submit code if we are on the last page
+		if nb.get_current_page() is nb.get_n_pages()-1:
+			email = self.get_email()
+			support_logs = self.get_support_logs()
+			support_msg = self.get_support_msg()
+			module = self.get_module()
+			print email, support_logs, support_msg, module  # debug print 
+			# We need to update this to use threading and asyncsubmit like canoe.py
+			#module.execute(email, support_msg, support_logs)
+			# We are exiting for now but we should give the user some confirmation beforehand
+			#sys.exit(0)
+
+		# Go to the next page if we aren't on the last page
+		else:
+			# Make the "back" button active if we are coming from the first page
+			if nb.get_current_page() is 0:
+				back.set_sensitive(True)
+
+			# Change the "next" button to a "finish" button if we are going to the last page
+			if nb.get_current_page() is nb.get_n_pages()-2:
+				next.set_label("gtk-ok")
+
+			nb.next_page()
+
+
+	# Helper functions
+	def get_email(self):
+		return self.wTree.get_widget("entry_email").get_text()
+
+	def get_support_logs(self):
+		log_dict = None
+		for section in self.sections_dict:
+			if self.sections_dict[section].get_active():
+				log_path = functions.get_conf_item("list", section, "file")
+				log_dict = functions.append_log(log_dict, log_path, section)
+		# See comment in canoe.py about returning None
+		# Currently, returning None causes an error :)
+		return log_dict 
+
+	def get_support_msg(self):
+		text_view = self.wTree.get_widget("textview_description")
 		m_buffer = text_view.get_buffer()
 		m_buffer_s_iter = m_buffer.get_start_iter()
 		m_buffer_e_iter = m_buffer.get_end_iter()
 		message = m_buffer.get_text(m_buffer_s_iter, m_buffer_e_iter)
-		dialog.destroy()
 		return message
-	else:
-		dialog.destroy()
-		return None
-	
-def get_support_type():
-	dialog = gtk.Dialog("Problem Category", None, gtk.DIALOG_MODAL, (gtk.STOCK_CANCEL, gtk.RESPONSE_REJECT, gtk.STOCK_OK, gtk.RESPONSE_ACCEPT))
-	# Pack instruction and text-entry into the dialog
-	label = gtk.Label("Select all boxes relevant to your problem: ")
-	dialog.vbox.pack_start(label, False, False, 5)
-	# Create some check buttons
-	# We scan the configuration so that everything works
-	
-	sections_dict = dict([(x, gtk.CheckButton(x, False)) for x in functions.get_conf_sections("list")])
 
-			
-	#network_check = gtk.CheckButton("Network", False)
-	#video_check = gtk.CheckButton("Video", False)
-	# Pack the check buttons into the vbox
-	#dialog.vbox.pack_start(network_check, False, False, 5)
-	#dialog.vbox.pack_start(video_check, False, False, 5)
-	for x in sections_dict:
-		dialog.vbox.pack_start(sections_dict[x])
-	
-	dialog.set_default_size(400, dialog.get_property("default-height"))
-	dialog.show_all()
-	
-	log_dict = None
-	if dialog.run() == gtk.RESPONSE_ACCEPT:
-		#support_list.append("standard")
-		#if network_check.get_active():
-		#	support_list.append("network")
-		#if video_check.get_active():
-		#	support_list.append("video")
-		for x in sections_dict:
-			if sections_dict[x].get_active():
-				log_path = functions.get_conf_item("list", x, "file")
-				log_dict = functions.append_log(log_dict, log_path, x)
-	# Mask is initialized to 0, so we can safely return it
-	# if the user didn't enter anything, since we will get false
-	dialog.destroy()
-	return log_dict
-
-def get_submit_module():
-	dialog = gtk.Dialog("Upload Server", None, gtk.DIALOG_MODAL, (gtk.STOCK_CANCEL, gtk.RESPONSE_REJECT, gtk.STOCK_OK, gtk.RESPONSE_ACCEPT))
-	label = gtk.Label("Select a server to upload your data to")
-	dialog.vbox.pack_start(label, False, False, 5)
-	
-	combobox = gtk.combo_box_new_text()
-	server_list = [module for module in module_loader]
-	for mod in server_list:
-		combobox.append_text(mod.module_name)
-	dialog.vbox.pack_start(combobox, False, False, 5)		
-	if server_list:
-		text_list = [gtk.Label(server_list[0].module_name), gtk.Label(server_list[0].module_description), gtk.Label(server_list[0].module_submit_url)] 
-	else:
-		text_list = [gtk.Label(""), gtk.Label(""), gtk.Label("")]
-		
-	for x in text_list:
-		dialog.vbox.pack_start(x, False, False, 10)
-		
-	combobox.connect("changed", submit_select_changed, text_list)
-	
-	dialog.show_all()
-	if dialog.run() == gtk.RESPONSE_ACCEPT:
-		module = module_loader.module(combobox.get_active_text())
-		dialog.destroy()
+	def get_module(self):
+		module = self.module_loader[self.module_combobox.get_active_text()]
 		return module
-	else:
-		return None
-		
-	
-	
 
-def submit_select_changed(widget, text_list):
-	mod_name = widget.get_active_text()
-	module = module_loader.module(mod_name)
-	text_list[0].set_text(module.module_name)
-	text_list[1].set_text(module.module_description)
-	text_list[2].set_text(module.module_submit_url)
-		
-def on_submit_complete(request_result, user_data=None):
-	global label
-	global window
-	global hbuttonbox
-	global button
-	gtk.threads_enter()	
-	label.set_text("The submission is complete")
-	button = gtk.Button("Close", gtk.STOCK_CLOSE);		
-	hbuttonbox.pack_end(button, False, False, 0)
-	button.connect_object("clicked", gtk.Widget.destroy, window)
-	button.show()
-	window.set_title("Finished")
-	gtk.threads_leave()
 
-# Only execute if this was called directly
+	def main(self):
+		gtk.main()
+	def destroy(self, widget):
+		gtk.main_quit()
+
 if __name__ == "__main__":
-	# Actual code
-	# Initialize threading
+	# Initialize
 	functions.set_conf_dir("../upstream-base/conf/")
-	module_loader = submitmoduleloader.SubmitModuleLoader(["../upstream-base/submit-modules"], False, submitmoduleloader.moduleloader.DEBUG_ALL)
-	gtk.threads_init()	
-	request = query()
-	if request:
-		submit(request)
+	module_loader = submitmoduleloader.SubmitModuleLoader(["../upstream-base/submit-modules"])
+
+	# Load the GUI
+	canoe = CanoeGTK(module_loader)
+	canoe.main()
