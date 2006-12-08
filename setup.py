8d6de9a60ac2445580ccfabb149172ea108320e5
Mahangu Weerasinghe <mahangu@users.noreply.github.com>
2006-12-08 02:16:24 +0000

    Initial check-in of setup.py script. Does not work.

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+#
+# Upstream - log file aggregator and report tool for *nix systems.
+# Build System for versions 0.3+
+#
+# Copyright (C) 2006  Mahangu Weerasinghe (mahangu@gmail.com)
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+from distutils.core import setup
+
+setup(name='Upstream',
+	version="0.3-alpha",
+	author="Mahangu Weerasighe",
+	packages=['upstream', 'log-modules', 'submit-modules'],
+	package_dir={'upstream' : 'upstream-base',
+			'log-modules' : 'upstream-base/log-modules',
+			'submit-modules' : 'upstream-base/submit-modules'
+			},
+	data_files=[('/usr/share/pixmaps', ['extras/upstream.png']),
+		  ('/usr/share/upstream', ['extras/canoe.glade']),
+                  ('/etc/upstream', ['conf/upstream.conf'])
+		  ])
+
+
