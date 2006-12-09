642a238d0dfa1c07b83724f3ad7dac11a27b969a
ryanzeigler <ryanzeigler@de486275-911b-0410-b142-b35c017fc177>
2006-12-09 13:13:29 +0000

    Alter pastebindotcom to function with the new framework

--- branch/advsubmit/upstream-base/submit-modules/pastebindotcom.py
+++ branch/advsubmit/upstream-base/submit-modules/pastebindotcom.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 #
 # Upstream pastebin.com  module.
-# Copyright (C) 2006  Jason Ribeiro <jason.ribeiro@gmail.com>
+# Copyright (C) 2006  Jason Ribeiro <jason.ribeiro@gmail.com>,
+# Ryan Zeigler <zeiglerr@gmail.com>
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -19,40 +20,38 @@
 
 
 import submitmoduleloader
+import messageframe
 from urllib import urlopen, urlencode
 
 module_name = "pastebindotcom"
 module_description = """Module for pastebin.com"""
 module_submit_url = "http://pastebin.com"
 
-
-def execute(submit_name, submit_message, dict_of_logs):
-	global module_submit_url
+def createSubmit(m_buffer):
+	return PastebindotcomPlugin(m_buffer)	
 	
-	print "Executing"
-
-	# Put all the elements into one log
-	flat_log_type = ""
-	for log in dict_of_logs:
-		flat_log_type = flat_log_type + "\n%s:\n\n%s" % (log, dict_of_logs[log])
-
-	# 'expiry' specifies for what period of time the paste should be kept
-	#	"d" - one day
-	#	"m" - one month (this is default on the web interface)
-	#	"f" - forever
-	post_data = { 'format' : "text", 'code2': submit_name + "\n\n" + submit_message + "\n\n" + flat_log_type, 'poster': "Upstream", 'paste': "Send", 'expiry': "m" }
-
-	# Send the data
-	try:
-		paste = urlopen(module_submit_url, urlencode(post_data))
-	except IOError:
-		return submitmoduleloader.SubmitModuleResult(True, False)
-
-	result_url = paste.geturl()
-	result_xml = paste.read()
-
-	print result_url
-
-	# TODO: We need to check that the page we get back actually has the logs
-
-	return submitmoduleloader.SubmitModuleResult(True, True, result_xml, result_url)
+class PastebindotcomPlugin(messageframe.SubmitPlugin):
+	module_submit_url = module_submit_url
+	def __init__(self, m_buffer):
+		messageframe.SubmitPlugin.__init__(m_buffer):
+		
+	def execute(self):		
+		print "Executing"
+		username, message, logs = self._fetchSubmitData()		
+		# Put all the elements into one log
+		flat_log_type = ""
+		for log in logs:
+			flat_log_type = flat_log_type + "\n%s:\n\n%s" % (log, logs[log])
+		# 'expiry' specifies for what period of time the paste should be kept
+		#	"d" - one day
+		#	"m" - one month (this is default on the web interface)
+		#	"f" - forever
+		post_data = { 'format' : "text", 'code2': username + "\n\n" + message + "\n\n" + flat_log_type, 'poster': "Upstream", 'paste': "Send", 'expiry': "m" }
+		# IOErrors are caught by the enclosing block, one shot attempt
+		paste = urlopen(self.module_submit_url, urlencode(post_data))
+		result_url = paste.geturl()
+		# This may not longer be necessary
+		result_xml = paste.read()	
+		print result_url	
+		# TODO: We need to check that the page we get back actually has the logs
+		self._m_buffer.pushBack(DoneMessage(result_url))
