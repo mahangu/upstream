6841d1245de74747430812cc482118fb1609a1ce
ryanzeigler <ryanzeigler@de486275-911b-0410-b142-b35c017fc177>
2007-03-07 03:50:35 +0000

    The OutputSynchronizer now correctly takes data. The new PluginLoader class successfully imports all modules.

--- trunk/upstream-base/functionality_test.py
+++ trunk/upstream-base/functionality_test.py
@@ -10,10 +10,13 @@ config.readfp(open('../conf/upstream.conf'))
 import pluginloader, uconf
 
 confdir = config.get("paths","confdir")
-
+osync = pluginloader.OutputSynchronizer(sys.stdout)
 l_config = uconf.PluginConfigReader(uconf.LOG, confdir)
-log_modules = pluginloader.PluginLoader(l_config, sys.stdout)
+log_modules = pluginloader.PluginLoader(l_config, osync )
+s_config = uconf.PluginConfigReader(uconf.SUBMIT, confdir)
+submit_modules = pluginloader.PluginLoader(s_config, osync)
 log_modules.start()
+submit_modules.start()
 log_modules.join()
-log_modules.wait()
-log_modules.dump_log()
+submit_modules.join()
+osync.dump()
